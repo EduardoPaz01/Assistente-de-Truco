30e4e30855baff671a1a4bf8a27eadeb439f4b0b
Eduardo Páz Putti <eduardopazputti@gmail.com>
2024-12-28 22:14:38 -0300

    Estrutura de recebimento de cartas disponíveis e jogadas

---
+++ data.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <list>
+
+const char naipes[4] = { 'P', 'C', 'E', 'O' };
+const int valores[10] = { 1, 2, 3, 4, 5, 6, 7, 10, 11, 12 };
+
+class Folha {
+public:
+    Folha(char naipe_, int valor_) : naipe(naipe_), valor(valor_) {}
+
+    int valor;
+    char naipe;
+};
+
+class Baralho {
+public:
+    std::list<Folha> folhas;
+};
