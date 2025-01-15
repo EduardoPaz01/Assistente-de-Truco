7eb6f0c4b3a7bcfd42ffc89e581aca54c08380c9
Eduardo Paz <eduardopazputti@gmail.com>
2025-01-14 22:35:07 -0300

    Revisão de código da main.js

--- javascript/main.js
+++ javascript/main.js
@@ -8,7 +8,7 @@ class Main {
 
         this.baralho = new Baralho()
         
-        this.janela = new Janela(" ")
+        this.janela = new Janela()
         
         this.comecoJogo = false
         this.turno_atual = 0
@@ -16,85 +16,130 @@ class Main {
 
     // Inicia a primeira tela
     inicio (){
-        this.janela.gerarMenuQuantidadeJogadores()
-        this.Equipes.push(new Equipe("blue"))
-        this.Equipes.push(new Equipe("red"))
-    }
-
-    setQuantidadeJogadores(numero_jogadores){
-        this.quantidade_jogadores = numero_jogadores
-
-        // Cria os novos jogadores e adiciona eles nas equipes
-        for(let i=0; i<this.quantidade_jogadores; i++){
-            this.Jogadores.push(new Jogador())
-            this.Equipes[i%2].setJogador(this.Jogadores[i])
-        }
-
-        // Configura o menu inferior de controle de cartas jogadas
-        this.janela.gerarMenuInferior(this.quantidade_jogadores)
-
-        // Configura a exibição das folhas do jogador
-        this.janela.gerarFolhasJogador()
-
-        // Configura o menu rotação
-        this.janela.gerarMenuRotacao(this.quantidade_jogadores)
-
-    }
-
-    setPosicaoJogador(posicao_jogador){
-        // Configura os times
-        for(let i=0; i<this.quantidade_jogadores; i++){
-            let suaEquipe = posicao_jogador%2
-            if(i==posicao_jogador)
-                this.Jogadores[i].setName("Você")
-            else if(i%2==suaEquipe)
-                this.Jogadores[i].setName("Colega")
-            else if(i%2!=suaEquipe)
-                this.Jogadores[i].setName("Adversário")
-        }
-
-        // Configura o mini menu
-        this.janela.gerarMenuRotacaoSuperior(posicao_jogador, this.quantidade_jogadores)
-        this.janela.updateMenuRotacaoSuperior(0)
-
-        // Começa o laço de repetição
         this.continue()
     }
 
     continue(){
+        // Define a quantidade de jogadores
+        if (!this.quantidade_jogadores){
+            this.definirQuantidadeJogadores()
+        }
+        //Define a posição do jogador
+        else if(!this.Jogadores[0].posicao){
+            this.definirPosicaoJogador()
+        }
         // Verificando se a mão do jogador tem menos de 3 cartas ou se é nula
-        if (this.Jogadores[0].mao.length < 3 || this.Jogadores[0].mao.length == null) {            
-            this.janela.coletarFolha("Selecione sua folha", this.baralho).then((folha) => {
-                this.Jogadores[0].setNovaFolha(folha)
-                this.baralho.setFolha(folha)
-                this.janela.updateFolhasJogador(this.Jogadores[0].getMao())
-                this.continue()
-            }).catch((erro) => { 
-                console.error("Erro ao coletar a folha:", erro)
-            })
+        else if (this.Jogadores[0].mao.length < 3 || !this.Jogadores[0].mao.length) {            
+            this.definirFolhaJogador()
         }
         // Coleta a manilha
-        else if(this.baralho.getManilha()==null){
-            this.janela.coletarFolha("Selecione a manilha", this.baralho).then((folha) => {
-                this.baralho.setFolha(folha)
-                this.baralho.setManilha(folha)
-                this.janela.updateManilha(this.baralho.getManilha())
-                this.continue()
-            }).catch((erro) => { 
-                console.error("Erro ao coletar a folha:", erro)
-            })
+        else if(!this.baralho.obterManilha){
+            this.defirnirManilhaJogo()
         }
         // Ciclo do jogo
         else {
             alert("play")
         }
+    }
+
+    // Define a quantidade de jogadores
+    definirQuantidadeJogadores(){
+
+        this.janela.coletarQuantidadeJogadores().then((quantidade_de_jogadores)=>{
+            // Define a quantidade de jogadores
+            this.quantidade_jogadores = quantidade_de_jogadores
+
+            // Define as equipes
+            this.Equipes.push(new Equipe("blue"))
+            this.Equipes.push(new Equipe("red"))
+    
+            // Cria os novos jogadores e adiciona eles nas equipes
+            for(let i=0; i<this.quantidade_jogadores; i++){
+                this.Jogadores.push(new Jogador())
+                this.Equipes[i%2].setJogador=this.Jogadores[i]
+            }
+    
+            // Configura o menu inferior de controle de cartas jogadas
+            this.janela.mostrarMenuInferior(this.quantidade_jogadores)
+    
+            // Configura a exibição das folhas do jogador
+            this.janela.mostrarFolhasJogador()
+
+            // Retoma a função principal
+            this.continue()
+        }).catch((erro) => { 
+            console.error("Erro ao coletar quantos jogadores estão jogando:", erro)
+        })
 
     }
+
+    // Define a posição do jogador
+    definirPosicaoJogador(){
+
+        this.janela.coletarPosicaoJogador(this.quantidade_jogadores).then((posicao_jogador)=>{
+            // Atualiza a posição do jogador
+            this.Jogadores[0].definirPosicao = posicao_jogador
+
+            // Configura os times
+            for(let i=0; i<this.quantidade_jogadores; i++){
+                let suaEquipe = posicao_jogador%2
+                if(i==posicao_jogador)
+                    this.Jogadores[i].setName="Você"
+                else if(i%2==suaEquipe)
+                    this.Jogadores[i].setName="Colega"
+                else if(i%2!=suaEquipe)
+                    this.Jogadores[i].setName="Adversário"
+            }
+
+            // Configura o mini menu
+            this.janela.mostrarMenuRotacaoSuperior(posicao_jogador, this.quantidade_jogadores)
+            this.janela.atualizarMenuRotacaoSuperior(0)
+
+            // Começa o laço de repetição
+            this.continue()
+        }).catch((erro) => { 
+            console.error("Erro ao selecionar a sua posição:", erro)
+        })
+    }
+
+    // Define uma nova folha para o jogador
+    definirFolhaJogador(){
+
+        this.janela.coletarFolha("Selecione sua folha", this.baralho).then((folha) => {
+            // Configura a entrada da folha
+            this.Jogadores[0].definirFolhaMao = folha
+            this.baralho.definirFolha = folha
+            this.janela.atualizarFolhasJogador(this.Jogadores[0].obterMao)
+
+            // Retoma o laço principal
+            this.continue()
+        }).catch((erro) => { 
+            console.error("Erro ao coletar a folha:", erro)
+        })
+
+    }
+
+    // Define a manilha
+    defirnirManilhaJogo(){
+
+        this.janela.coletarFolha("Selecione a manilha", this.baralho).then((folha) => {
+            // Configura a manilha
+            this.baralho.definirFolha = folha
+            this.baralho.definirManilha = folha
+            this.janela.atualizarManilha(this.baralho.obterManilha)
+
+            // Retoma o laço
+            this.continue()
+        }).catch((erro) => { 
+            console.error("Erro ao coletar a folha:", erro)
+        })
+    }
+
 }
 
  // Configurações da janela
 class Janela {
-    constructor(menuId){
+    constructor(){
         this.container = null
     }
 
@@ -103,8 +148,48 @@ class Janela {
         this.container.replaceChildren()
     }
 
+    // Produz o menu de seleção de quantidade de jogadores
+    coletarQuantidadeJogadores(){
+        // Limpa a tela
+        this.container = document.getElementById("interface_principal")
+        this.limparInterface()
+
+        return new Promise((resolve, reject) => {
+            // Configura a pergunta
+            let pergunta = document.createElement("h1")
+            pergunta.innerHTML = "Quantas pessoas estão jogando?"
+
+            // Configura os botões de seleção de quantidade de jogadores
+            let espaco_botoes = document.createElement("div")
+            for(let i=0; i<2; i++){
+                let butao = document.createElement("button")
+                let imagem = document.createElement("img")
+                imagem.src = "imagens/"+((i%2==0)?"quatrojogadores":"seisjogadores")+".png"
+                butao.setAttribute("title", ((i%2==0)?"4 jogadores":"6 jogadores") )
+                butao.onclick = () => { resolve((i%2==0)?4:6) }
+                butao.appendChild(imagem)
+                espaco_botoes.append(butao)
+            }
+            // Adiciona ao container
+            this.container.appendChild(pergunta)
+            this.container.appendChild(espaco_botoes)
+        })
+    }
+
+    // Gera o menu de controle de folhas jogadas
+    mostrarMenuInferior(quantidade_jogadores){
+        const menuInferior = document.getElementById("rodape")
+        
+        for(let i=0; i<quantidade_jogadores; i++){
+            const folha = document.createElement("img")
+            folha.src = "imagens/folhas/vazia.png"
+            folha.id = `folha_pilha${i}`
+            menuInferior.appendChild(folha)
+        }
+    }
+
     // Gera o menu das folhas do jogador
-    gerarFolhasJogador(){
+    mostrarFolhasJogador(){
         const folhasJogador = document.getElementById("estado_sua_mao")
         folhasJogador.replaceChildren()
         
@@ -116,106 +201,55 @@ class Janela {
         }
     }
 
-    // Atualiza o menu das folhas do jogador
-    updateFolhasJogador(mao_do_jogador){
-
-        let i=0
-        for(let folha of mao_do_jogador){
-            let folha_menu = document.getElementById(`folhasJogador${i}`)
-            folha_menu.src = folha.getCaminhoFolha()
-            i++
-        }
-
-    }
-
-    // Atualiza a impressão da manilha
-    updateManilha(folha){
-        const manilha = document.getElementById("manilha")
-        manilha.replaceChildren()
-
-        const img_manilha = document.createElement("img")
-        img_manilha.src = folha.getCaminhoFolha()
-        manilha.appendChild(img_manilha)
-        manilha.style.border = "5px solid purple"
-    }
-
-    // Produz o menu de seleção de quantidade de jogadores
-    gerarMenuQuantidadeJogadores(){
-        this.container = document.getElementById("interface_principal")
-        this.limparInterface()
-
-        // Configura a pergunta
-        let pergunta = document.createElement("h1")
-        pergunta.innerHTML = "Quantas pessoas estão jogando?"
-
-        // Configura os botões de seleção de quantidade de jogadores
-        let espaco_botoes = document.createElement("div")
-        for(let i=0; i<2; i++){
-            let butao = document.createElement("button")
-            let imagem = document.createElement("img")
-            imagem.src = "imagens/"+((i%2==0)?"quatrojogadores":"seisjogadores")+".png"
-            
-            butao.setAttribute("title", ((i%2==0)?"4 jogadores":"6 jogadores") )
-            butao.onclick = ()=>{controle.setQuantidadeJogadores((i%2==0)?4:6)}
-            butao.appendChild(imagem)
-            espaco_botoes.append(butao)
-        }
-        this.container.appendChild(pergunta)
-        this.container.appendChild(espaco_botoes)
-    }
-
     // Corrija a janela de seleção da posição do jogador
-    gerarMenuRotacao(quantidade_jogadores){
+    coletarPosicaoJogador(quantidade_jogadores){
         
         //Limpa a tela
         this.limparInterface()
-        
-        // Configura a pergunta
-        let pergunta = document.createElement("h1")
-        pergunta.innerHTML = "Qual a sua posição?"
+        return new Promise((resolve, reject) => {
+            // Configura a pergunta
+            let pergunta = document.createElement("h1")
+            pergunta.innerHTML = "Qual a sua posição?"
 
-        let linha_sup = document.createElement("div") 
-        let linha_inf = document.createElement("div") 
-        for(let i=0; i<quantidade_jogadores; i++){
-            // Cria o botão
-            const button = document.createElement("button")
+            // Configura os botões
+            let linha_sup = document.createElement("div") 
+            let linha_inf = document.createElement("div") 
+            for(let i=0; i<quantidade_jogadores; i++){
+                // Cria o botão
+                const butao = document.createElement("button")
+                const img = document.createElement("img")
+                img.src = "imagens/"+((i==0)?"dealer":"umjogador")+".png"
+
+                //Finaliza o botão
+                butao.onclick = () => { resolve(correcaoRotacao(i, quantidade_jogadores)) }
+                butao.appendChild(img)
+
+                // Adiciona os botões nas respectivas filas
+                if(i<quantidade_jogadores/2)
+                    linha_sup.appendChild(butao)
+                else if(i>=quantidade_jogadores/2)
+                    linha_inf.appendChild(butao)
+            }
+            // Configura a mesa
             const img = document.createElement("img")
-            img.src = "imagens/"+((i==0)?"dealer":"umjogador")+".png"
+            img.id = "mesa"
+            img.src = "imagens/mesa.png"
+            
+            // Posiciona os elementos
+            let espaco_botoes = document.createElement("div") 
+            espaco_botoes.id = "rotacao"
+            espaco_botoes.appendChild(linha_sup)
+            espaco_botoes.appendChild(img)
+            espaco_botoes.appendChild(linha_inf)
 
-            //Finaliza o botão
-            button.onclick = ()=>{ controle.setPosicaoJogador( correcaoRotacao(i, quantidade_jogadores) ) }
-            button.appendChild(img)
-
-            if(i<quantidade_jogadores/2)
-                linha_sup.appendChild(button)
-            else if(i>=quantidade_jogadores/2)
-                linha_inf.appendChild(button)
-        }
-        // Configura a mesa
-        const img = document.createElement("img")
-        img.id = "mesa"
-        img.src = "imagens/mesa.png"
-        
-        // Posiciona os elementos
-        let espaco_botoes = document.createElement("div") 
-        espaco_botoes.id = "rotacao"
-        espaco_botoes.appendChild(linha_sup)
-        espaco_botoes.appendChild(img)
-        espaco_botoes.appendChild(linha_inf)
-
-        //Adiciona a interface
-        this.container.append(pergunta)
-        this.container.appendChild(espaco_botoes)
+            //Adiciona a interface
+            this.container.append(pergunta)
+            this.container.appendChild(espaco_botoes)
+        })       
     }
 
-    // Atualiza o menu de rotação por rodada
-    updateMenuRotacaoSuperior(posicao_do_jogo){
-        let imgJogador = document.getElementById(`user_pos${posicao_do_jogo}`)
-        imgJogador.style.border = "1px solid orangered"
-    }
-
-    // Formate o menu de rotacao superior pela primeira vez
-    gerarMenuRotacaoSuperior(posicao_do_jogador, quantidade_jogadores){
+    // Mostre o menu de rotacao superior pela primeira vez
+    mostrarMenuRotacaoSuperior(posicao_do_jogador, quantidade_jogadores){
 
         //Configura o menu superior de rotação
         const menu_rot = document.getElementById('rotacao_mini')
@@ -248,19 +282,12 @@ class Janela {
         // Marca a posição do jogador
         let imgJogador = document.getElementById(`user_pos${posicao_do_jogador}`)
         imgJogador.style.backgroundColor = "yellow"
-
     }
 
-    // Gera o menu de controle de folhas jogadas
-    gerarMenuInferior(quantidade_jogadores){
-        const menuInferior = document.getElementById("rodape")
-        
-        for(let i=0; i<quantidade_jogadores; i++){
-            const folha = document.createElement("img")
-            folha.src = "imagens/folhas/vazia.png"
-            folha.id = `folha_pilha${i}`
-            menuInferior.appendChild(folha)
-        }
+    // Atualiza o menu de rotação por rodada
+    atualizarMenuRotacaoSuperior(posicao_do_jogo){
+        let imgJogador = document.getElementById(`user_pos${posicao_do_jogo}`)
+        imgJogador.style.border = "1px solid orangered"
     }
 
     // Coleta uma folha
@@ -318,12 +345,12 @@ class Janela {
                     let linha_inf = document.createElement("div")
 
                     // Limita para aparecer apenas as folhas disponíveis
-                    const folhas_disponiveis = baralho.getfolhas(naipe)
+                    const folhas_disponiveis = baralho.obterfolhas(naipe)
                     let j=0
                     for(let folha of folhas_disponiveis){
                         let butaoValor = document.createElement("button")
                         const imagem = document.createElement("img")
-                        imagem.src = folha.getCaminhoFolha()
+                        imagem.src = folha.obterCaminhoFolha
                         butaoValor.appendChild(imagem)
                         butaoValor.onclick = () => { resolve(folha) }
                         if(j < folhas_disponiveis.length/2)
@@ -355,6 +382,29 @@ class Janela {
         })
     }
 
+    // Atualiza o menu das folhas do jogador
+    atualizarFolhasJogador(mao_do_jogador){
+
+        let i=0
+        for(let folha of mao_do_jogador){
+            let folha_menu = document.getElementById(`folhasJogador${i}`)
+            folha_menu.src = folha.obterCaminhoFolha
+            i++
+        }
+
+    }
+
+    // Atualiza a impressão da manilha
+    atualizarManilha(folha){
+        const manilha = document.getElementById("manilha")
+        manilha.replaceChildren()
+
+        const img_manilha = document.createElement("img")
+        img_manilha.src = folha.obterCaminhoFolha
+        manilha.appendChild(img_manilha)
+        manilha.style.border = "5px solid purple"
+    }
+
 }
 
 
@@ -366,7 +416,7 @@ class Folha{
         this.valor = valor
     }
 
-    getCaminhoFolha() {
+    get obterCaminhoFolha() {
         return `imagens/folhas/${this.naipe}${this.valor}.png`
     }
 
@@ -389,21 +439,22 @@ class Baralho {
         }
     }
 
-    setManilha(folha){
+    set definirManilha(folha){
         this.manilha = folha
     }
-    getManilha(folha){
+
+    get obterManilha(){
         return this.manilha
     }
 
-    setFolha(nova_folha){
+    set definirFolha(nova_folha){
         const index = this.folhas.findIndex(folha => folha.naipe == nova_folha.naipe && folha.valor == nova_folha.valor)
         if (index !== -1){ 
             this.folhas.splice(index, 1)
         }
     }
     
-    getfolhas(naipe){
+    obterfolhas(naipe){
         if (naipe == null)
             return this.folhas
         
@@ -429,18 +480,18 @@ class Jogador {
         this.mao = []
     }
 
-    setNovaFolha( nova_folha ){
+    set definirFolhaMao( nova_folha ){
         this.mao.push( nova_folha )
     }
 
-    getMao(){
+    get obterMao(){
         return this.mao
     }
 
-    setName(name){
+    set definirName(name){
         this.name = name
     }
-    setPosicao(posicao){
+    set definirPosicao(posicao){
         this.posicao = posicao
     }
 
@@ -450,11 +501,11 @@ class Jogador {
 class Equipe {
     constructor(name){
         this.name = name
-        this.puntuacao = 0
+        this.pontuacao = 0
         this.Jogadores = []
     }
 
-    setJogador(novoJogador){
+    set definirJogador(novoJogador){
         this.Jogadores.push(novoJogador)
     }
 }
@@ -462,10 +513,10 @@ class Equipe {
 ////////////////////////////////////////////////////////////////////////////////
 
 //Definição de Variáveis
-var controle = new Main();
+var controle_jogo = new Main();
 
 // Mostrar o menu principal ao carregar a página
-window.addEventListener('load', ()=>{ controle.inicio() } )
+window.addEventListener('load', ()=>{ controle_jogo.inicio() } )
 
 // Fator de correção para o valor ddas folhas
 function correcaoSequencia(indice){
